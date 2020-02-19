3a6498e4494dd8a6b23e877f34ec277d93e14b4d
Erika Takahara <erika.takahara+erikatakahara@gmail.com>
2020-02-19 00:51:12 -0300

    [re-frame] Creates a simple toggle switch component

--- src/cljs/re_frame_study/views.cljs
+++ src/cljs/re_frame_study/views.cljs
@@ -2,10 +2,22 @@
   (:require
    [re-frame.core :as re-frame]
    [re-frame-study.subs :as subs]
-   ))
+   [reagent.core :as r]))
+
+(defn toggle-switch [_]
+  (let [selected* (r/atom false)
+        id (str "toggle-switch-" (rand 1000))]
+    (fn [label]
+      [:div.toggle-switch
+       [:input {:type :checkbox
+                :checked @selected*
+                :on-change (fn [_] (swap! selected* not))
+                :id id}]
+       [:label {:for id} (str label ": " (if @selected* "Active" "Inactive"))]])))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
-     [:h1 "Hello from " @name]
-     ]))
+     [:h1 "Features"]
+     [toggle-switch "Feature 1"]
+     [toggle-switch "Feature 2"]]))
