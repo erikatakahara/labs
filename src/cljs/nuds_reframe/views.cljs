e884104e7c608cf52ec70f8886442d0dd771b4bd
Erika Takahara <erika.takahara@gmail.com>
2020-06-10 11:57:19 -0300

    Add mui components

--- src/cljs/nuds_reframe/views.cljs
+++ src/cljs/nuds_reframe/views.cljs
@@ -1,21 +1,36 @@
 (ns nuds-reframe.views
   (:require
-   [re-frame.core :as re-frame]
+   [re-frame.core :as rf]
+   [reagent.core :as r]
    [nuds-reframe.subs :as subs]
    ["@material-ui/core" :as mui]
    ["@material-ui/icons" :as mui-icons]))
 
 (defn main-panel []
-  (let [name (re-frame/subscribe [::subs/name])]
-    [:div
-     [:h1 "Hello from " @name]
-     [:> mui/Button
-      {:variant :contained
-       :color   :primary}
-      "Hey"
-      [:> mui-icons/KeyboardArrowRight]]
-     [:div
-      [:> mui/FormControl
-       [:> mui/InputLabel {:html-for "my-input"} "Email address"]
-       [:> mui/Input {:id "my-input" :aria-describedby "my-helper-text"}]
-       [:> mui/FormHelperText {:id "my-helper-text"} "We'll never share your email."]]]]))
+  (let [name         (rf/subscribe [::subs/name])
+        person-email*  (r/atom "")]
+    (fn []
+      [:> mui/Box
+       [:> mui/Typography {:variant :h3
+                           :component :h1}
+        "Hello from " @name]
+       [:> mui/Box {:display :flex}
+        [:> mui/Box {:flex 1}
+         [:> mui/Typography {:variant :h4
+                             :component :h4} "Person form"]
+
+         [:> mui/FormControl
+          [:> mui/InputLabel {:html-for "email"} "Email address"]
+          [:> mui/Input {:id               "email"
+                         :aria-describedby "email-tip"
+                         :value            @person-email*
+                         :on-change        (fn [e]
+                                             (js/console.log (-> e .-target .-value))
+                                             (reset! person-email* (-> e .-target .-value)))}]
+          [:> mui/FormHelperText {:id "email-tip"} "We'll never share your email."]]]
+
+        [:> mui/Box {:flex 1}
+         [:> mui/Typography {:variant :h4
+                             :component :h4}"Person data"]
+         [:> mui/Typography {:component :p} "E-mail"]
+         [:> mui/Typography {:color "textSecondary" :component :p} @person-email*]]]])))
