2164b804d1cd97a67844a61dca0d3f5aecac03b6
Erika Takahara <erika.takahara@gmail.com>
2020-06-10 09:17:42 -0300

    Remove reframe components for the tests

--- src/cljs/re_frame_study/views.cljs
+++ src/cljs/re_frame_study/views.cljs
@@ -4,20 +4,7 @@
    [re-frame-study.subs :as subs]
    [reagent.core :as r]))
 
-(defn toggle-switch [_]
-  (let [selected* (r/atom false)
-        id (str "toggle-switch-" (rand 1000))]
-    (fn [label]
-      [:div.toggle-switch
-       [:input {:type :checkbox
-                :checked @selected*
-                :on-change #(swap! selected* not)
-                :id id}]
-       [:label {:for id} (str label ": " (if @selected* "Active" "Inactive"))]])))
-
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
-     [:h1 "Features"]
-     [toggle-switch "Feature 1"]
-     [toggle-switch "Feature 2"]]))
+     [:h1 "NuDS on Re-frame"]]))
